454ad5df4ba33039dd29c15a3a65da17cb3bb400
David Nidever <dnidever@lsst.org>
2019-07-15 23:35:32 -0600

    Major updates to nsc_instcal_combine.py.

--- python/nsc_instcal_combine.py
+++ python/nsc_instcal_combine.py
@@ -6,10 +6,84 @@ import numpy as np
 import warnings
 from astropy.io import fits
 from astropy.utils.exceptions import AstropyWarning
-from astropy.table import Table, vstack
+from astropy.table import Table, vstack, Column
 from astropy.time import Time
 import healpy as hp
 from dlnpyutils import utils
+import subprocess
+time
+
+def add_elements(cat,nnew=300000L):
+    """ Add more elements to a catalog"""
+    ncat = len(cat)
+    old = cat.copy()
+    nnew = utils.gt(nnew,ncat)
+    cat = np.zeros(ncat+nnew,dtype=old.dtype)
+    cat[0:ncat] = old
+    del(old)
+    return cat
+    
+def add_cat(obj,totobj,idstr,cnt,idcnt,ind1,cat,meta):
+    """ Add object information from a new meas catalog of matched objects"""
+
+    f = meta['filter'].lower()
+    # Copy to final structure
+    obj[ind1]['ra'] += cat['ra']
+    obj[ind1]['dec'] += cat['dec']
+    obj[ind1]['raerr'] += 1.0/cat['raerr']**2                           # total(ra_wt)
+    obj[ind1]['decerr'] += 1.0/cat['decerr']**2                         # total(dec_wt)
+    obj[ind1]['pmra'] += (1.0/cat['raerr']**2) * meta.mjd*cat['ra']     # total(wt*mjd*ra)
+    obj[ind1]['pmdec'] += (1.0/cat['decerr']**2) * meta.mjd*cat['dec']  # total(wt*mjd*dec)
+    obj[ind1]['mjd'] += meta['mjd']                                 # total(mjd)
+    obj[ind1]['ndet'] += 1
+    # Detection and morphology parameters for this FILTER
+    obj[obj]['ndet'+f] += 1
+    obj[obj][f+'asemi'] += cat['asemi']
+    obj[obj][f+'bsemi'] += cat['bsemi']
+    obj[obj][f+'theta'] += cat['theta']
+    # Good photometry for this FILTER
+    gdmag, = np.where(cat['mag_auto']<50)
+    if len(gdmag)>0:
+      obj[ind1[gdmag]][f+'mag'] += 2.5118864d**cat[gdmag]['mag_auto'] * (1.0/cat[gdmag]['magerr_auto']**2)
+      obj[ind1[gdmag]][f+'err'] += 1.0/cat[gdmag]['magerr_auto']**2
+      obj[ind1[gdmag]]['nphot'+f] += 1
+    obj[ind1]['asemi'] += cat['asemi']
+    obj[ind1]['asemierr'] += cat['asemierr']**2
+    obj[ind1]['bsemi'] += cat['bsemi']
+    obj[ind1]['bsemierr'] += cat['bsemierr']**2
+    obj[ind1]['theta'] += cat['theta']
+    obj[ind1]['thetaerr'] += cat['thetaerr']**2
+    obj[ind1]['fwhm'] += cat['fwhm']  # in arcsec
+    obj[ind1]['flags'] += cat['flags']
+    obj[ind1]['class_star'] += cat['class_star']
+    totobj[ind1]['ra'] += cat['ra'] * (1.0/cat['raerr']**2)             # total(ra*wt)
+    totobj[ind1]['dec'] += cat['dec'] * (1.0/cat['decerr']**2)          # total(dec*wt)
+    totobj[ind1]['ramjd'] += (1.0/cat['raerr']**2) * meta['mjd']        # total(wt_ra*mjd)
+    totobj[ind1]['decmjd'] += (1.0/cat['decerr']**2) * meta['mjd']      # total(wt_dec*mjd)
+    totobj[ind1]['ramjd2'] += (1.0/cat['raerr']**2) * meta['mjd']**2    # total(wt_ra*mjd**2)
+    totobj[ind1]['decmjd2'] += (1.0/cat['decerr']**2) * meta['mjd*']**2  # total(wt_dec*mjd**2)
+    totobj[ind1]['minmjd'] = utils.lt( meta['mjd'], totobj[ind1]['minmjd'] )
+    totobj[ind1]['maxmjd'] = utils.gt( meta['mjd'], totobj[ind1]['maxmjd'] )
+    if len(gdmag)>0:
+        totobj[ind1[gdmag]][f+'tot'] += cat[gdmag]['mag_auto']       # sum(mag)
+        totobj[ind1[gdmag]][f+'mag2'] += np.float64(cat[gdmag]['mag_auto'])**2   # sum(mag**2), need dbl to precent underflow
+    cnt += ncat
+
+    # Add new elements to IDSTR
+    if idcnt+ncat gt nidstr:
+        idstr = add_elements(idstr)
+        nidstr = len(idstr)
+
+    # Add to IDSTR
+    idstr[idcnt:idcnt+ncat]['measid'] = cat['measid']
+    idstr[idcnt:idcnt+ncat]['exposure'] = meta['base']
+    idstr[idcnt:idcnt+ncat]['expnum'] = meta['expnum']
+    idstr[idcnt:idcnt+ncat]['objectid'] = newexp['objectid']
+    idstr[idcnt:idcnt+ncat]['objectindex'] = ind1
+    idcnt += ncat
+
+    return obj,totobj,idstr,cnt,idcnt
+    
 
 def loadmeas(metafile,buffdict=None):
 
@@ -103,6 +177,7 @@ if __name__ == "__main__":
     #parser.add_argument('--pixfiles', type=str, default=False, help='IDL program')
     args = parser.parse_args()
 
+    t0 = time.time()
     hostname = socket.gethostname()
     host = hostname.split('.')[0]
 
@@ -231,6 +306,7 @@ if __name__ == "__main__":
                           ('fwhm',float),('flags',int),('class_star',float),('ebv',float)])
     tags = dtype_obj.names
     obj = np.zeros(500000,dtype=dtype_obj)
+    obj['pix'] = pix
     nobj = len(obj)
     dtype_totobj = np.dtype([('ra',np.float64),('dec',np.float64),('ramjd',np.float64),('decmjd',np.float64),('ramjd2',np.float64),
                              ('decmjd2',np.float64),('minmjd',np.float64),('maxmjd',np.float64),('umag2',np.float64),('gmag2',np.float64),
@@ -244,6 +320,7 @@ if __name__ == "__main__":
     cnt = 0L
 
     # Loop over the exposures
+    allmeta = []
     for i in range(nlist):
         print(str(i+1)+' Loading '+list[i]['FILE'])
 
@@ -328,5 +405,261 @@ if __name__ == "__main__":
                                ('nchips',long),('badchip31',bool),('rarms',float),('decrms',float),
                                ('ebv',float),('gaianmatch',long),('zpterm',float),('zptermerr',float),
                                ('refmatch',long)])
-        #STRUCT_ASSIGN,meta,newmeta
-        #PUSH,allmeta,newmeta
+        newmeta = np.zero(1,dtype=dtype_meta)
+        # Copy over the meta information
+        for n in newmeta.names:
+            if n in meta.names: newmeta[n]=meta[n]
+        allmeta.append(newmeta)
+
+        # Combine the data
+        #-----------------
+        # First catalog
+        if cnt==0:
+            ind1 = np.arange(len(cat))
+            obj,totobj,idstr,cnt,idcnt = add_cat(obj,totobj,idstr,cnt,idcnt,ind1,cat,meta)
+            obj[ind1]['objectid'] = str(pix)+'.'+str(np.arange(ncat)+1)
+
+
+        # Second and up
+        else:
+            #  Match new sources to the objects
+            ind1,ind2 = xmatch(obj[0:cnt]['ra'],obj[0:cnt]['dec'],cat['ra'],cat['dec'],0.5)
+            nmatch = len(ind1)
+            #  Some matches, add data to existing record for these sources
+            if nmatch>0:
+                obj,totobj,idstr,cnt,idcnt = add_cat(obj,totobj,idstr,cnt,idcnt,ind1,cat[ind2],meta)
+                if nmatch<ncat:
+                    cat = np.delete(cat,ind2)
+                    ncat = len(cat)
+                else:
+                    cat = np.array([])
+                    ncat = 0
+
+            # Some left, add records for these sources
+            if ncat>0:
+                print('  '+str(ncat)+' sources left to add')
+                # Add new elements
+                if (cnt+ncat)>nobj:
+                    obj = add_elements(obj)
+                    nobj = len(obj)
+                ind1 = np.arange(ncat)
+                obj,totobj,idstr,cnt,idcnt = add_cat(obj,totobj,idstr,cnt,idcnt,ind1,cat,meta)
+                obj[ind1]['objectid'] = str(pix)+'.'+str(cnt+np.arange(ncat)+1)
+
+    # No sources
+    if cnt==0:
+        print('No sources in this pixel')
+        sys.exit()
+    # Trim off the excess elements
+    obj = obj[0:cnt]
+    totobj = totobj[0:cnt]
+    nobj = len(obj)
+    print(str(nobj)+' final objects')
+    idstr = idstr[0:idcnt]
+
+    # Make NPHOT from NPHOTX
+    obj['nphot'] = obj['nphotu']+obj['nphotg']+obj['nphotr']+obj['nphoti']+obj['nphotz']+obj['nphoty']+obj['nphotvr']
+
+    # Convert total(mjd*ra) to true proper motion values
+    #  the slope of RA vs. MJD is
+    #  pmra=(total(wt*mjd*ra)/total(wt)-<mjd>*<ra>)/(total(wt*mjd^2)/total(wt)-<mjd>^2)
+    #  we are creating the totals cumulatively as we go
+    totobj['ra'] /= obj['raerr']        # wt mean RA (totalrawt/totalwt)
+    totobj['dec'] /= obj['decerr']      # wt mean DEC (totaldecwt/totalwt)
+    obj['mjd'] /= obj['ndet']           # mean MJD
+    totobj['ramjd'] /= obj['raerr']     # wt_ra mean MJD
+    totobj['decmjd'] /= obj['decerr']   # wt_dec mean MJD
+    pmra = (obj['pmra']/obj['raerr']-totobj['ramjd']*totobj['ra'])/(totobj['ramjd2']/obj['raerr']-totobj['ramjd']**2)   # deg[ra]/day
+    pmra *= (3600*1e3)*365.2425     # mas/year
+    pmra *= np.cos(obj['dec']/radeg)      # mas/year, true angle
+    pmdec = (obj['pmdec']/obj['decerr']-totobj['decmjd']*totobj['dec'])/(totobj['decmjd2']/obj['decerr']-totobj['decmjd']**2)  # deg/day
+    pmdec *= (3600*1e3)*365.2425    # mas/year
+    # Proper motion errors
+    # pmerr = 1/sqrt( sum(wt*mjd^2) - <mjd>^2 * sum(wt) )
+    #   if wt=1/err^2 with err in degrees, but we are using arcsec
+    #   Need to divide by 3600 for PMDECERR and 3600*cos(dec) for PMRAERR
+    pmraerr = 1.0/np.sqrt( totobj['ramjd2'] - totobj['ramjd']**2 * obj['raerr'] )
+    pmraerr /= (3600*np.cos(totobj['dec']/radeg))    # correction for raerr in arcsec
+    pmraerr *= (3600*1e3)*365.2425     # mas/year
+    pmraerr *= np.cos(obj['dec']/radeg)      # mas/year, true angle
+    pmdecerr = 1.0/np.sqrt( totobj['decmjd2'] - totobj['decmjd']**2 * obj['decerr'] )
+    pmdecerr /= 3600                   # correction for decerr in arcsec
+    pmdecerr *= (3600*1e3)*365.2425    # mas/year
+    gdet, = np.where(obj['ndet']>1)
+    ngdet = len(gdet)
+    if ngdet>0:
+        obj[gdet]['pmra'] = pmra[gdet]
+        obj[gdet]['pmdec'] = pmdec[gdet]
+        obj[gdet]['pmraerr'] = pmraerr[gdet]
+        obj[gdet]['pmdecerr'] = pmdecerr[gdet]
+    bdet, = np.where((obj['ndet']<2) | ~np.isfinite(pmra))
+    nbdet = len(bdet)
+    # sometimes it happens that the denominator is 0.0 
+    #  when there are few closely spaced points
+    #  nothing we can do, just mark as bad
+    if nbdet>0:
+        obj[bdet]['pmra'] = 999999.0
+        obj[bdet]['pmdec'] = 999999.0
+        obj[bdet]['pmraerr'] = 999999.0
+        obj[bdet]['pmdecerr'] = 999999.0
+    obj['deltamjd'] = totobj['maxmjd']-totobj['minmjd']
+    # Average coordinates
+    obj['ra'] = totobj['ra']   # now stuff in the average coordinates
+    obj['dec'] = totobj['dec']
+    obj['raerr'] = np.sqrt(1.0/obj['raerr'])    # err in wt mean RA, arcsec
+    obj['decerr'] = np.sqrt(1.0/obj['decerr'])  # err in wt mean DEC, arcsec
+
+
+    # Convert totalwt and totalfluxwt to MAG and ERR
+    #  and average the morphology parameters PER FILTER
+    filters = ['u','g','r','i','z','y','vr']
+    for f in filters:
+        newflux = obj[f+'mag'] / obj[f+'err']
+        newmag = 2.50*np.log10(newflux)
+        newerr = np.sqrt(1.0/obj[f+'err'])
+        obj[f+'mag'] = newmag
+        obj[f+'err'] = newerr
+        bdmag, = np.where(~np.isfinite(newmag))
+        nbdmag = len(bdmag)
+        if nbdmag>0:
+            obj[bdmag][f+'mag'] = 99.99
+            obj[bdmag][f+'err'] = 9.99
+
+        # Calculate RMS scatter
+        #  RMS^2 * N = sum(mag^2) - 2*<mag>*sum(mag) + N*<mag>^2
+        #   where <mag> is a weighted average
+        #  RMS = sqrt( sum(mag^2)/N - 2*<mag>*sum(mag)/N + <mag>^2 )
+        #  sum(mag^2) is in the MAG2 column and sum(mag) is in TOT
+        rms = np.zeros(nobj,float)
+        gdrms, = np.where(obj['nphot'+f]>1)
+        ngdrms = len(gdrms)
+        bdrms, = np.where(obj['nphot'+f]<=1)
+        nbdrms = len(bdrms)
+        if ngdrms>0:
+           rms[gdrms] = np.sqrt( totobj[gdrms][f+'mag2']/obj[gdrms]['nphot'+f] - $
+                                 2*obj[gdrms][f+'mag']*totobj[gdrms][f+'tot']/obj[gdrms]['nphot'+f] + obj[gdrms][f+'mag'])**2 )
+        if nbdrms>0: rms[bdrms] = 999999.
+        obj[f+'rms'] = rms
+
+        # Average the morphology parameters PER FILTER
+        gdet, = np.where(obj['ndet'+f]>0)
+        ngdet = len(gdet)
+        bdet, = np.where(obj['ndet'+f]==0)
+        nbdet = len(bdet)        
+        if ngdet>0:
+            obj[gdet][f+'asemi'] /= obj[gdet]['ndet'+f]
+            obj[gdet][f+'bsemi'] /= obj[gdet]['ndet'+f]
+            obj[gdet][f+'theta'] /= obj[gdet]['ndet'+f]
+        if nbdet>0:
+            obj[bdet][f+'asemi'] = 99.99
+            obj[bdet][f+'bsemi'] = 99.99
+            obj[bdet][f+'theta'] = 99.99
+
+    # Average the morphology parameters, Need a separate counter for that maybe?
+    mtags = ['asemi','bsemi','theta','fwhm','class_star']
+    gdet, = np.where(obj['ndet']>0)
+    ngdet = len(gdet)
+    bdet, = np.where(obj['ndet']==0)
+    nbdet = len(bdet)    
+    for m in mtags:
+        # Divide by the number of detections
+        if ngdet>0: obj[gdet][m] /= obj[gdet]['ndet']
+        if nbdet>0: obj[bdet][m] = 99.99   # no good detections
+
+    # Get the average error
+    metags = ['asemierr','bsemierr','thetaerr']
+    for m in metags:
+        # Just take the sqrt to complete the addition in quadrature
+        if ngdet>0: obj[gdet][m] = np.sqrt(obj[gdet][m])
+        if nbdet>0: obj[bdet][m] = 99.99
+
+    # Add E(B-V)
+    print('Getting E(B-V)')
+    #glactc,obj.ra,obj.dec,2000.0,glon,glat,1,/deg
+    #obj['ebv'] = DUST_GETVAL(glon,glat,/noloop,/interp)
+
+    # ONLY INCLUDE OBJECTS WITH AVERAGE RA/DEC
+    # WITHIN THE BOUNDARY OF THE HEALPIX PIXEL!!!
+    ipring = hp.pixelfunc.ang2pix(nside,obj['ra'],obj['dec'],lonlat=True)
+    ind1, = np.where(ipring == pix)
+    nmatch = len(ind1)
+    if nmatch==0:
+        print('None of the final objects fall inside the pixel')
+        sys.exit()
+    # Get trimmed objects and indices
+    objtokeep = np.zeros(nobj,bool)         # boolean to keep or trim objects
+    objtokeep[ind1] = True
+    if nmatch<nobj:
+        trimind = np.arange(nobj)
+        trimind = np.delete(trimind,ind1)
+        #trimind = utils.remove_indices(trimind,ind1)
+        trimobj = obj[trimind]          # trimmed objects
+    newobjindex = np.zeros(nobj,long)-1    # new indices
+    newobjindex[ind1] = np.arange(nmatch)
+    # Keep the objects inside the Healpix
+    obj = obj[ind1]
+    print(str(nmatch)+' final objects fall inside the pixel')
+
+    # Remove trimmed objects from IDSTR
+    totrim, = np.where(objtokeep[idstr.objectindex] eq 0,ntotrim)  #using old index
+    if ntotrim>0:
+        # Trim objects
+        idstr = np.delete(idstr,totrim)
+        #idstr = utils.remove_indices(idstr,totrim)
+        # Update IDSTR.objectindex
+        old_idstr_objectindex = idstr['objectindex']
+        idstr['objectindex'] = newobjindex[old_idstr_objectindex]
+
+    # Create final summary structure from ALLMETA
+    #  get exposures that are in IDSTR
+    #  sometimes EXPNUM numbers have the leading 0s removed
+    #  and sometimes not, so turn to LONG to match
+    dum, uiexpnum = np.unique(np.long(idstr['expnum']),return_index=True)
+    uexpnum = np.long(idstr[uiexpnum]['expnum'])
+    nuexpnum = len(uexpnum)
+    ind1,ind2 = utils.match(np.long(allmeta.expnum),uexpnum)
+    nmatch = len(ind1)
+    sumstr = Table(allmeta[ind1])
+    col_nobj = Column(name='nobjects', dtype=np.long)
+    col_healpix = Column(name='healpix', dtype=np.long)
+    sumstr.add_columns([col_nobj col_healpix])
+    sumstr['nobjects'] =
+    sumstr['healpix'] = pix
+    # get number of objects per exposure
+    expnum = np.long(idstr.expnum)
+    siexp = np.sort(expnum)
+    expnum = expnum[siexp]
+    if nuexpnum>1:
+        brklo, = np.where(expnum != np.roll(expnum,1))
+        nbrk = len(brklo)
+        brkhi = np.hstack((brklo[1:nbrk-1],len(expnum)))
+        numobjexp = brkhi-brklo+1
+    else:
+        numobjexp=len(expnum)
+    ind1,ind2 = utils.match(np.long(sumstr.expnum),uexpnum)
+    nmatch = len(ind1)
+    sumstr[ind1]['nobjects'] = numobjexp
+
+    # Write the output file
+    print('Writing combined catalog to '+outfile)
+    if os.path.exists(outdir) is False: os.mkdir(outdir)
+    if os.path.exists(outdir+'/'+subdir) is False: os.mkdir(outdir+'/'+subdir)
+    if os.path.exists(outfile): os.delete(outfile)
+    sumstr.write(outfile)               # first, summary table
+    #  append other fits binary tables
+    hdulist = fits.open(outfile)
+    hdu = fits.table_to_hdu(obj)        # second, catalog
+    hdulist.append(hdu)
+    hdu = fits.table_to_hdu(idstr)      # third, ID table
+    hdulist.append(hdu)    
+    hdulist.writeto(outfile,overwrite=True)
+    hdulist.close()
+    #MWRFITS,sumstr,outfile,/create      # first, summary table
+    #MWRFITS,obj,outfile,/silent         # second, catalog
+    #MWRFITS,idstr,outfile,/silent       # third, ID table
+    if os.path.exists(outfile+'.gz'): os.delete(outfile)    
+
+    ret = subprocess.call(['gzip',outfile])    # compress final catalog
+
+    dt = time.time()-t0
+    print('dt = '+str(dt)+' sec.')
