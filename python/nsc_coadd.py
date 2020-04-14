6a6a6cd31dd2728418aa669cbbf9d960d705c79b
David Nidever <dnidever@lsst.org>
2020-04-14 16:13:15 -0600

    Updates to nsc_coadd.py

--- python/nsc_coadd.py
+++ python/nsc_coadd.py
@@ -28,9 +28,10 @@ import time
 import glob
 import logging
 import sep
+import healpy as hp
 from reproject import reproject_interp
 import tempfile
-from dlnpyutils import utils as dln
+from dlnpyutils import utils as dln, coords
 
 
 def rootdirs():
@@ -39,10 +40,10 @@ def rootdirs():
     shost = host.split('.')[0]
 
     if shost == 'thing' or shost == 'hulk':
-        return ('/net/dl1','/mss1/','/data0/')
+        return ('/net/dl2','/mss1','/data0')
 
     if shost == 'gp06' or shost == 'gp07' or shost == 'gp08' or shost == 'gp09':
-        return ('/net/dl1','/net/mss1/','/data0/')
+        return ('/net/dl2','/net/mss1','/data0')
 
     
 def brickwcs(ra,dec,npix=3600,step=0.262):
@@ -120,6 +121,9 @@ def image_interp(imagefile,outhead,weightfile=None,masknan=False):
         
     # Loop over the HDUs
     for i in range(nimhdu):           
+
+        # CHECK THAT IT OVERLAPS THE FINAL AREA!!!
+
         mask = None
         # Flux image
         im = imhdulist[i].data
@@ -460,9 +464,69 @@ def coadd(imagefiles,weightfiles,meta,outhead,coaddtype='average'):
     # could also use swarp
     # https://www.astromatic.net/pubsvn/software/swarp/trunk/doc/swarp.pdf
 
-def nsc_coadd(brick,band='g'):
+def getbrickexposures(brick,band=None,version='v3'):
+    """ Get exposures information that overlap a brick."""
+
+    dldir, mssdir, localdir = rootdirs()
+    brick_dbfile = dldir+'/dnidever/nsc/instcal/'+version+'/lists/nsc_bricks.db'
+    brickdata = db.query(brick_dbfile, table='bricks', cols='*', where='brickname="'+brick+'"')
+
+    # Healpix information
+    pix128 = hp.ang2pix(128,brickdata['ra'],brickdata['dec'],lonlat=True)
+    # neighbors
+    neipix = hp.get_all_neighbours(128,pix128)
+    
+    # Get all of the exposures overlapping this region
+    meta_dbfile = dldir+'/dnidever/nsc/instcal/'+version+'/lists/nsc_meta.db'
+    allpix = np.hstack((neipix.flatten(),pix128))
+    whr = ' or '.join(['ring128=='+h for h in allpix.astype(str)])
+    chipdata = db.query(meta_dbfile, table='chip', cols='*', where=whr)
+
+    # Do more overlap checking
+    brick_vra = np.hstack((brickdata['ra1'],brickdata['ra2'],brickdata['ra2'],brickdata['ra1']))
+    brick_vdec = np.hstack((brickdata['dec1'],brickdata['dec1'],brickdata['dec2'],brickdata['dec2']))    
+    olap = np.zeros(len(chipdata),bool)
+    for i in range(len(chipdata)):
+        vra = np.hstack((chipdata['vra1'][i],chipdata['vra2'][i],chipdata['vra2'][i],chipdata['vra1'][i]))
+        vdec = np.hstack((chipdata['vdec1'][i],chipdata['vdec1'][i],chipdata['vdec2'][i],chipdata['vdec2'][i]))
+        olap[i] = coords.doPolygonsOverlap(vra,vdec,brick_vra,brick_vdec)
+    ngdch = np.sum(olap)
+    if ngdch==0:
+        print('No exposures overlap brick '+brick)
+        return None
+    chipdata0 = chipdata.copy()
+    chipdata = chipdata[olap]
+    exposure = np.unique(chipdata['exposure'])
+    nexp = len(exposure)
+    print(str(nexp)+' exposures overlap brick '+brick)
+
+    # Get the exosure data
+    whr = ' or '.join(['exposure=="'+e+'"' for e in exposure.astype(str)])
+    expdata = db.query(meta_dbfile, table='exposure', cols='*', where=whr)
+
+    # Check band
+    if band is not None:
+        gband, = np.where(expdata['filter']==band)
+        if len(gband)==0:
+            print('No '+band+' exposures overlap brick '+brick)
+            return None
+        expdata = expdata[gband]
+    
+    return expdata
+    
+    
+def nsc_coadd(brick,band='g',version='v3'):
     pass
     # This creates a coadd for one NSC brick
 
     # Make sure to fix the WCS using the coefficients I fit with Gaia DR2
     #  that are in the meta files.
+
+    expdata = getbrickexposures(brick,band=band,version=version)
+    
+    dldir, mssdir, localdir = rootdirs()
+    brick_dbfile = dldir+'/dnidever/nsc/instcal/'+version+'/lists/nsc_bricks.db'
+    brickdata = db.query(brick_dbfile, table='bricks', cols='*', where='brickname="'+brick+'"')
+
+    # Healpix information
+    pix128 = hp.ang2pix(128,brickdata['ra'],brickdata['dec'],lonlat=True)
