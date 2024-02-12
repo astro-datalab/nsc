a9a9cbfbc7a8f48c95fc23d7360ebbade5e47aa6
Katie Fasbender <37817649+katiefasbender@users.noreply.github.com>
2024-02-12 13:59:18 -0800

    Update exposure_copy.py

--- python/exposure_copy.py
+++ python/exposure_copy.py
@@ -9,12 +9,13 @@
 #    explist - fits file with table of exposures to copy/transfer, with
 #              fluxfile, wtfile, and maskfile columns
 #    transfer_lim - maximum amount of data [MB] per transfer (sets the number
-#                   of exposures per repository)
+#                   of exposures per sub-repository)
+#    nside - HEALPix nside to sort healpix into; each has 
 
-# The script will create a directory called "copy_group/YYYYMMDDHHMMSS/" in
-# your current working directory, and in there will be X number of repositories
-# with Y exposures each.  Each repository contains all the files for one manual
-# Globus transfer of <transfer_lim> MB
+# The script will create a directory called "exp_copies/YYYYMMDDHHMMSS/" in
+# your current working directory, and in within each healpix subdir there
+# will be X number of "transfer repositories" with Y exposures each.  Each "transfer repository"
+# contains all the files for one manual Globus transfer of <transfer_lim> MB
 
 #-----------------------------------------------------------------------------
 # Imports
@@ -25,6 +26,7 @@ from astropy.time import Time,TimezoneInfo
 from astropy.table import *
 import astropy.units as u
 from datetime import datetime,timezone
+import healpy as hp
 import numpy as np
 import os
 import socket
@@ -64,6 +66,7 @@ if __name__ == "__main__":
     parser = ArgumentParser(description='Copy exposure filess from "explist" to several common repositories (of a set size "transfer_lim") to transfer with Globus from DataLab to CCA servers')
     parser.add_argument('--explist',type=str, nargs=1, default="decam_exposures_tomeas.fits.gz",help='Filename with list of exposures with rawname,flux/wt/maskfile columns (must be in same directory as script)')
     parser.add_argument('--transfer_lim',type=str,nargs=1,default="12000",help="max amount of data per transfer, in MBytes (default 4000, determines number of exposures (3 files each) per repo)") # each file is ~250-400 MB, so 1 exposure <1200MB of data
+    parser.add_argument('--nside',type=str,nargs=1,default="32",help="HEALPix nside to split exposures into") 
     args = parser.parse_args()
 
     # Get input arguments
@@ -71,6 +74,7 @@ if __name__ == "__main__":
     print("explistfile = ",explistfile)
     transfer_limit = int(args.transfer_lim)  # max amount of data per transfer [MBytes]
     print("transfer limit = ",transfer_limit)
+    ns = args.nside
 
     # Start time, get host & local directory
     t00 = time.time()
@@ -81,7 +85,7 @@ if __name__ == "__main__":
     localdir = os.getcwd()                      # where this script is
     mssdir = "/net/mss1/"                       # where exposures are stored on dl machines
     ####mssdir = "/home/group/davidnidever/nsc/instcal/v4/exposures/"
-    copydir = localdir+"/copy_groups/"+timestamp+"/"          # where exposures will be copied to, divided
+    copydir = localdir+"/exposure_copies/"+timestamp+"/"          # where exposures will be copied to, divided
     makedir(copydir)                                            # into one repo per planned transfer
 
     # ----------------------------------------
@@ -91,30 +95,37 @@ if __name__ == "__main__":
     # Read in list, check for missing columns
     exposures = Table.read(explistfile)         # list of exposures
     expcols = exposures.colnames                # columns
-    for col in ["RAWNAME","FLUXFILE","WTFILE","MASKFILE"]:
+    for col in ["RAWNAME","FLUXFILE","WTFILE","MASKFILE","RA","DEC"]:
         if col not in expcols:
             print("missing colum ",col," from exposure input list ",explistfile,"!!!")
             sys.exit()
+    exposures['ring'+str(ns)] = hp.ang2pix(nside=ns,exposures['RA'],exposures['DEC'],lonlat=True)
+    nhpix = hp.nside2npix(ns)
 
 
-    # Group exposures into transfer batches
+    # Group exposures into subdirs (1 per hpix) and within that, into further subdirs (1 per transfer batch)
     nexposures = len(exposures)                 # Number of exposures
     nfiles = nexposures*3                       # Number of exposure files
     filesize = 400                              # max exposure file size [MB]
     exposuresize = filesize*3
     nexposures_per_repo = transfer_limit//exposuresize  # Nmber of files per repository
-    nrepos = (nexposures//nexposures_per_repo)+1        # Number of repositories/transfers
-    subdir_list = np.array([copydir+"expgroup_"+str(i).zfill(len(str(nrepos))) for i in range(nrepos)]) # list of subdirectories in copydir
-    print("splitting ",nexposures," exposures into ",nrepos," repositories")
-    exposures['copydir'] = [subdir_list[(i-nrepos*(i//nrepos))] for i in range(nexposures)]
-    print(exposures['copydir'])
+    ####nrepos = (nexposures//nexposures_per_repo)+1        # Number of repositories/transfers
+    ####subdir_list = np.array([copydir+"expgroup_"+str(i).zfill(len(str(nrepos))) for i in range(nrepos)]) # list of subdirectories in copydir
+    subdir_list = np.array([copydir+"ring32_"+str(i).zfill(len(str(nhpix))) for i in range(nhpix)]) # list of subdirectories in copydir
+    print("splitting ",nexposures," exposures into ",np.unique(subdir_list)," HPix repositories")
+    #exposures['copydir'] = [subdir_list[(i-nrepos*(i//nrepos))] for i in range(nexposures)]
+    #print(exposures['copydir'])
 
 
     # Copy exposure files, grouped in directories by transfer batch
     for sbdr in subdir_list:
         makedir(sbdr)
-        transnum = exposures['copydir'] == sbdr
-        for exp in exposures[transnum]:
+        #transnum = exposures['copydir'] == sbdr
+        hpexposures = exposures['ring'+str(ns)] == sbdr
+        nrepos = (len(hpexposures)//nexposures_per_repo)+1
+        rcount = 0
+        #for exp in exposures[transnum]:
+        for exp in exposures[hpexposures]:
             # Change the root directory name
             #  /net/mss1/blah/blah/
             fluxfile = exp['FLUXFILE'].strip()
@@ -132,10 +143,12 @@ if __name__ == "__main__":
             # Copy flux,wt,maskfiles from /net/mss1/blah to new common repo
             for fl in [fluxfile,wtfile,maskfile]:
                 src = fl
-                dst = sbdr+"/"+fl.split("/")[-1]
+                dst = sbdr+"/transfer_"+str(rcount)+"/"+fl.split("/")[-1]
                 if os.path.exists(fl):
                     shutil.copy(src,dst)
                 else: print(fl," missing!")
+            rcount+=1
+            rcount = rcount-(rcount//nrepos)*nrepos
 
     print(time.time()-t00," seconds")
     efile = copydir+explistfile
