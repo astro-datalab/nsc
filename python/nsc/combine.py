b4c23fe04d9ea96516974768dc1ef380b2991914
David Nidever <dnidever@montana.edu>
2025-02-19 17:18:29 -0600

    updated seqclusterpm() function in combine.py

--- python/nsc/combine.py
+++ python/nsc/combine.py
@@ -423,30 +423,33 @@ def seqpms(obj):
     slpra[twodet] = (sumtra[twodet]/ndet[twodet]-mnra[twodet]*mnt[twodet]) / denom
     slpdec[twodet] = (sumtdec[twodet]/ndet[twodet]-mndec[twodet]*mnt[twodet]) / denom
     
-    return mnra,mndec,slpra,slpdec
+    return mnt,mnra,mndec,slpra,slpdec
     
-def seqclusterpm(tab,dcr=0.5,doiter=False,inpobj=None,trim=False):
+def seqclusterpm(meas,dcr=0.5,doiter=False,inpobj=None,calcpm=True,trim=False):
     """
     Sequential clustering of measurements in exposures with proper motion.
+    If you are rerunning with a previous object table and do NOT want to
+    to recalculate the proper motions (as we go), then set calcpm=False
     """
 
-    ntab = len(tab)
-    labels = np.zeros(ntab)-1
+    nmeas = len(meas)
+    labels = np.zeros(nmeas)-1   # object label (also its index) for all the measurements
 
     # Create exposures index
-    index = dln.create_index(tab['EXPOSURE'])
+    index = dln.create_index(meas['EXPOSURE'])
     nexp = len(index['value'])
 
     # Create object catalog
     dtype_obj = np.dtype([('label',int),('ra',np.float64),('dec',np.float64),('ndet',int),
                           ('sumt',float),('sumt2',float),('sumra',float),('sumdec',float),
-                          ('sumtra',float),('sumtdec',float)])
+                          ('sumtra',float),('sumtdec',float),('slpra',float),
+                          ('slpdec',float),('mnt',float)])
     # Is there an input object catalog that we are starting with?
     if inpobj is not None:
-        obj = inpobj
+        obj = inpobj.copy()
         cnt = len(obj)
     else:
-        obj = np.zeros(np.min([500000,ntab]),dtype=dtype_obj)
+        obj = np.zeros(np.min([500000,nmeas]),dtype=dtype_obj)
         cnt = 0
     nobj = len(obj)
 
@@ -454,8 +457,9 @@ def seqclusterpm(tab,dcr=0.5,doiter=False,inpobj=None,trim=False):
     for i in range(nexp):
         #print(str(i)+' '+index['value'][i])
         indx = index['index'][index['lo'][i]:index['hi'][i]+1]
-        tab1 = tab[indx]
-        ntab1 = len(tab1)
+        meas1 = meas[indx]
+        nmeas1 = len(meas1)
+        measmjd = meas1['mjd'][0]
         if dln.size(dcr)>1:
             dcr1 = dcr[indx]
         else:
@@ -463,75 +467,91 @@ def seqclusterpm(tab,dcr=0.5,doiter=False,inpobj=None,trim=False):
         
         # First exposure
         if cnt==0:
-            ind1 = np.arange(ntab1)
+            ind1 = np.arange(nmeas1)
             obj['label'][ind1] = ind1
-            obj['ra'][ind1] = tab1['RA']
-            obj['dec'][ind1] = tab1['DEC']
+            obj['ra'][ind1] = meas1['RA']
+            obj['dec'][ind1] = meas1['DEC']
             obj['ndet'][ind1] = 1
-            obj['sumt'][ind1] = tab1['MJD']
-            obj['sumt2'][ind1] = tab1['MJD']
-            obj['sumra'][ind1] = tab1['RA']
-            obj['sumdec'][ind1] = tab1['DEC']
-            obj['sumtra'][ind1] = tab1['MJD']*tab1['RA']
-            obj['sumtdec'][ind1] = tab1['MJD']*tab1['DEC']
+            obj['sumt'][ind1] = meas1['MJD']
+            obj['sumt2'][ind1] = meas1['MJD']
+            obj['sumra'][ind1] = meas1['RA']
+            obj['sumdec'][ind1] = meas1['DEC']
+            obj['sumtra'][ind1] = meas1['MJD']*meas1['RA']
+            obj['sumtdec'][ind1] = meas1['MJD']*meas1['DEC']
+            obj['slpra'][ind1] = 0.0
+            obj['slpdec'][ind1] = 0.0
+            obj['mnt'][ind1] = measmjd
             labels[indx] = ind1
-            cnt += ntab1
+            cnt += nmeas1
 
         # Second and up, or object catalog input
         else:
-            ind2,ind1,dist = coords.xmatch(tab1['RA'],tab1['DEC'],obj[0:cnt]['ra'],
-                                           obj[0:cnt]['dec'],dcr1,unique=True)            
+            # Predict current coordinates with linear fit
+            # this is helpful if there is a big temporal gap between
+            # two exposures
+            predra = obj['ra'][:cnt] + obj['slpra'][:cnt]*(measmjd-obj['mnt'][:cnt])
+            preddec = obj['dec'][:cnt] + obj['slpdec'][:cnt]*(measmjd-obj['mnt'][:cnt])
+
+            ind2,ind1,dist = coords.xmatch(meas1['RA'],meas1['DEC'],predra,
+                                           preddec,dcr1,unique=True)
+            #ind2,ind1,dist = coords.xmatch(meas1['RA'],meas1['DEC'],obj[:cnt]['ra'],
+            #                               obj[:cnt]['dec'],dcr1,unique=True)
             nmatch = dln.size(ind1)
             #  Some matches, add data to existing records for these measurements
             if nmatch>0:
                 obj['ndet'][ind1] += 1
-                obj['sumt'][ind1] += tab1['MJD'][ind2]
-                obj['sumt2'][ind1] += tab1['MJD'][ind2]
-                obj['sumra'][ind1] += tab1['RA'][ind2]
-                obj['sumdec'][ind1] += tab1['DEC'][ind2]
-                obj['sumtra'][ind1] += tab1['MJD'][ind2]*tab1['RA'][ind2]
-                obj['sumtdec'][ind1] += tab1['MJD'][ind2]*tab1['DEC'][ind2]
-                mnra,mndec,slpra,slpdec = seqpms(obj[ind1])
-                # Predict current coordinates with linear fit
-                thismjd = tab1['MJD'][ind2[0]]
-                predra = mnra+slpra*(thismjd-mnt)
-                preddec = mndec+slpdec*(thismjd-mnt)    
-                obj['ra'][ind1] = predra    # update coordinates
-                obj['dec'][ind1] = preddec
+                obj['sumt'][ind1] += meas1['MJD'][ind2]
+                obj['sumt2'][ind1] += meas1['MJD'][ind2]
+                obj['sumra'][ind1] += meas1['RA'][ind2]
+                obj['sumdec'][ind1] += meas1['DEC'][ind2]
+                obj['sumtra'][ind1] += meas1['MJD'][ind2]*meas1['RA'][ind2]
+                obj['sumtdec'][ind1] += meas1['MJD'][ind2]*meas1['DEC'][ind2]
+                # Calculate mean coordinates and proper motions
+                if calcpm:
+                    mnt,mnra,mndec,slpra,slpdec = seqpms(obj[ind1])
+                    obj['ra'][ind1] = mnra
+                    obj['dec'][ind1] = mndec
+                    obj['slpra'][ind1] = slpra
+                    obj['slpdec'][ind1] = slpdec
+                    obj['mnt'][ind1] = mnt
                 labels[indx[ind2]] = ind1
-                if nmatch<ntab1:
+                if nmatch<nmeas1:
                     indx0 = indx.copy()
                     indx = np.delete(indx,ind2)
-                    tab1 = np.delete(tab1,ind2)
-                    ntab1 = dln.size(tab1)
+                    meas1 = np.delete(meas1,ind2)
+                    nmeas1 = dln.size(meas1)
                 else:
-                    tab1 = np.array([])
-                    ntab1 = 0
+                    meas1 = np.array([])
+                    nmeas1 = 0
 
             # Some left, add records for these sources
-            if ntab1>0:
+            if nmeas1>0:
                 # Add new elements
-                if (cnt+ntab1)>nobj:
+                if (cnt+nmeas1)>nobj:
                     obj = add_elements(obj)
                     nobj = len(obj)
-                ind1 = np.arange(ntab1)+cnt
+                ind1 = np.arange(nmeas1)+cnt
                 obj['label'][ind1] = ind1
-                obj['ra'][ind1] = tab1['RA']
-                obj['dec'][ind1] = tab1['DEC']
-                obj['sumt'][ind1] = tab1['MJD']
-                obj['sumt2'][ind1] = tab1['MJD']
-                obj['sumra'][ind1] = tab1['RA']
-                obj['sumdec'][ind1] = tab1['DEC']
-                obj['sumtra'][ind1] = tab1['MJD']*tab1['RA']
-                obj['sumtdec'][ind1] = tab1['MJD']*tab1['DEC']
+                obj['ra'][ind1] = meas1['RA']
+                obj['dec'][ind1] = meas1['DEC']
+                obj['sumt'][ind1] = meas1['MJD']
+                obj['sumt2'][ind1] = meas1['MJD']
+                obj['sumra'][ind1] = meas1['RA']
+                obj['sumdec'][ind1] = meas1['DEC']
+                obj['sumtra'][ind1] = meas1['MJD']*meas1['RA']
+                obj['sumtdec'][ind1] = meas1['MJD']*meas1['DEC']
                 obj['ndet'][ind1] = 1
+                obj['slpra'][ind1] = 0.0
+                obj['slpdec'][ind1] = 0.0
+                obj['mnt'][ind1] = measmjd
+
                 labels[indx] = ind1
 
-                cnt += ntab1
+                cnt += nmeas1
     # Trim off the excess elements
-    obj = obj[0:cnt]
+    obj = obj[:cnt]
     # Trim off any objects that do not have any detections
-    #  could happen if an object catalog was input
+    #   could happen if an object catalog was input
     if trim is True:
         bd, nbd = dln.where(obj['ndet']<1)
         if nbd>0: obj = np.delete(obj,bd)
