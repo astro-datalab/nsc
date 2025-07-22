5da79cdc0447104a149d3845c5754b0cf7935938
David Nidever <dnidever@montana.edu>
2025-07-22 12:33:14 -0500

    fixed headfile issue in calibrate.py

--- python/nsc/calibrate.py
+++ python/nsc/calibrate.py
@@ -1018,10 +1018,14 @@ def calibrate(expdir,inpref=None,eqnfile=None,redo=False,selfcal=False,
                                             'header',instrument,night,base2+'_*.hdr'))
                 if len(headfile)>0:
                     headfile = headfile[0]
+                else:
+                    headfile = ''
         if os.path.exists(headfile)==False:
             # use instcal files on tacc
             #/home1/09970/dnidever/scratch1/nsc/instcal/v4/images/c4d/2020/20200130
-            headfile = os.path.dirname(expdir.replace(version,version+'/images'))+'/'+base+'.fits.fz'
+            headfile = '/home1/09970/dnidever/scratch1/nsc/instcal/v4/images/'
+            headfile += '/'.join(expdir.split('/')[-4:])+'.fits.fz'
+            #headfile = os.path.dirname(expdir.replace(version,version+'/images'))+'/'+base+'.fits.fz'
         if os.path.exists(headfile)==False:
             raise ValueError(headfile+' not found')
         headdict = loadheader(headfile)
